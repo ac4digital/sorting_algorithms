c02333f375836a6f542c8ebf9933310d79959a12
Andrés Cataño <3866@holbertonschool.com>
2022-02-11 20:32:24 -0500

    Bubble Sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Sorting algorithm
+ * @array: array of elements to sorting
+ * @size: size of array
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+    size_t i, j, tmp, count;
+    i = 0;
+    count = size;
+    tmp = 0;
+
+    while(array && i < size - 1)
+    {
+        j = 0;
+        while(j < count -1)
+        {
+            if (array[j] > array[j + 1])
+            {
+                tmp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = tmp;
+                print_array(array, size); 
+            }
+        j++;
+        }
+    count--;
+    i++;
+    }
+}
