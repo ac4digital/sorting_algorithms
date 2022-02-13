3cfd4d4fe41dd38a9987a783f655fc720b1dc56d
Andrés Cataño <3866@holbertonschool.com>
2022-02-13 14:26:34 -0500

    Fix Betty Style

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,26 +8,27 @@
 
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j, tmp, count;
-    i = 0;
-    count = size;
-    tmp = 0;
+	size_t i, j, tmp, count;
 
-    while(array && i < size)
-    {
-        j = 0;
-        while(j < count -1)
-        {
-            if (array[j] > array[j + 1])
-            {
-                tmp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = tmp;
-                print_array(array, size);
-            }
-        j++;
-        }
-    count--;
-    i++;
-    }
+	i = 0;
+	count = size;
+	tmp = 0;
+
+	while (array && i < size)
+	{
+		j = 0;
+		while (j < count - 1)
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				print_array(array, size);
+			}
+			j++;
+		}
+		count--;
+		i++;
+	}
 }
